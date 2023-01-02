9b6af816e3b8fcf4c73e72e218ef3fe6023f7663
facu408 <121831250+facu408@users.noreply.github.com>
2023-01-02 18:42:52 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,32 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (MARIO.vy == 0) {
+        MARIO.vy = -150
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+	
+})
+let MARIO: Sprite = null
+scene.setBackgroundColor(9)
+MARIO = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f f . . . . . 
+    . . . f f e e e e f 2 f . . . . 
+    . . f f e e e e f 2 2 2 f . . . 
+    . . f e e e f f e e e e f . . . 
+    . . f f f f e e 2 2 2 2 e f . . 
+    . . f e 2 2 2 f f f f e 2 f . . 
+    . f f f f f f f e e e f f f . . 
+    . f f e 4 4 e b f 4 4 e e f . . 
+    . f e e 4 d 4 1 f d d e f . . . 
+    . . f e e e e e d d d f . . . . 
+    . . . . f 4 d d e 4 e f . . . . 
+    . . . . f e d d e 2 2 f . . . . 
+    . . . f f f e e f 5 5 f f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . f f . . . f f f . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(MARIO, 100, controller.dx())
+tiles.setCurrentTilemap(tilemap`nivel1`)
+MARIO.ay = 200
+scene.cameraFollowSprite(MARIO)
